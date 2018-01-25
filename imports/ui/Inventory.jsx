1757a9f6294f7ff30fbe86a03380640dafa4a106
Indira Pranabudi <indirapranabudi@gmail.com>
2018-01-24 21:39:48 -0500

    Create stubs for Inventory and Recipe

---
+++ imports/ui/Inventory.jsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class Iventory extends Component {
+	render() {
+		return (<h1>Iventory</h1>);
+	}
+}
---
+++ imports/ui/Recipe.jsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class Recipe extends Component {
+	render() {
+		return (<h1>Recipe</h1>);
+	}
+}
