6c7a8e89fec2e2b4c623639e6523d28fe5497d0c
Zack Apiratitham <zack@vatthikorn.com>
2018-01-27 22:24:47 -0500

    Added Inventory component

--- imports/ui/Inventory.jsx
+++ imports/ui/Inventory.jsx
@@ -1,7 +1,33 @@
+import { Meteor } from 'meteor/meteor';
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Inventories } from '../api/inventories';
 
 export default class Inventory extends Component {
-	render() {
-		return (<h1>Inventory</h1>);
+	
+	deleteThisInventory() {
+		Meteor.call('inventories.remove', this.props.inventory._id);
 	}
+	
+	render() {
+		
+		return (
+			<li>
+				<button className="delete" onClick={this.deleteThisInventory.bind(this)}>
+					&times;
+				</button>
+
+				<span className="text">{this.props.inventory.name} - {this.props.inventory.amount}</span>
+			</li>
+		);
+	}
+}
+
+Inventory.propTypes = {
+	Inventories: PropTypes.shape({
+		name: PropTypes.string,
+		amount: PropTypes.string,
+		_id: PropTypes.string
+	})
 }
