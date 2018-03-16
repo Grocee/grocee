08b23891176be046a92f6251969dd578eab9ccb7
Zack Apiratitham <zack@vatthikorn.com>
2018-03-15 22:26:53 -0400

    Make amount optional when adding item
    - Added method to update amount for existing items

--- imports/api/inventories.js
+++ imports/api/inventories.js
@@ -6,13 +6,12 @@ export const Inventories = new Mongo.Collection('inventories');
 
 // If use arrow function this.userId will return undefined
 Meteor.publish('inventories', function() {
-	return Inventories.find({ owner: this.userId });
+	return Inventories.find({ owner: this.userId }); //, { sort: { createdAt: -1 }});
 });
 
 Meteor.methods({
-	'inventories.insert'(name, amount) {
+	'inventories.insert'(name) {
 		check(name, String);
-		check(amount, String);
 
 		// Make sure the user is logged in before inserting
 		if (!this.userId) {
@@ -23,16 +22,30 @@ Meteor.methods({
 			throw new Meteor.Error('name cannot be empty')
 		}
 
+		Inventories.insert({
+			name: name,
+			owner: this.userId,
+			createdAt: new Date(),
+		});
+	},
+	'inventories.updateAmount'(itemId, amount) {
+		check(itemId, String);
+		check(amount, String);
+
+		if (!this.userId) {
+			throw new Meteor.Error('not-authorized');
+		}
+
+		if (itemId.length === 0) {
+			throw new Meteor.Error('itemId cannot be empty')
+		}
+
 		if (amount.length === 0) {
 			throw new Meteor.Error('amount cannot be empty')
 		}
 
-		Inventories.insert({
-			name: name,
-			amount: amount,
-			owner: this.userId,
-			createdAt: new Date(),
-		});
+		Inventories.update({_id: itemId}, {$set: {amount: amount}});
+
 	},
 	'inventories.remove'(itemId) {
 		check(itemId, String);
