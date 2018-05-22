fbaf6351d68a5ad6ff75c7307641acd4e97af3d2
Indira Pranabudi <indirapranabudi@gmail.com>
2018-05-21 21:15:16 -0400

    Make authorization more generic

--- utils/authorization.js
+++ utils/authorization.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
-const authCheck = (collection, userId, groceryId) => {
+const authCheck = (collection, userId, id) => {
 	// Make sure only the owner can delete
-	const grocery = collection.findOne(groceryId);
-	if (grocery.owner !== userId) {
+	const item = collection.findOne(id);
+	if (item.owner !== userId) {
 		throw new Meteor.Error('not-authorized');
 	}
 }
