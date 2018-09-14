46f3ee11c8417cf6779e5c93a481a2b95ae3e81f
Zack Apiratitham <zack@vatthikorn.com>
2018-09-14 15:01:21 -0400

    initial commit

--- imports/api/inventories.js
+++ imports/api/inventories.js
@@ -38,8 +38,10 @@ Meteor.methods({
 
 		InventoryLists.update(listId, { $push: { items: newItem } });
 	},
-	'inventories.update'(itemId, name, amount = null) {
+	'inventories.update'(itemId, name, listId, amount = null) {
 		check(itemId, String);
+		check(name, String);
+		check(listId, String);
 
 		if (itemId.length === 0) {
 			throw new Meteor.Error('itemId cannot be empty')
@@ -49,6 +51,13 @@ Meteor.methods({
 			throw new Meteor.Error('Name cannot be empty')
 		}
 
+		if (listId.length === 0) {
+			throw new Meteor.Error('listId cannot be empty')
+		}
+
+		console.log(`itemId: ${listId}`);
+
+		authCheck(InventoryLists, this.userId, listId);
 		authCheck(Inventories, this.userId, itemId);
 
 		if (amount != null) {
@@ -58,7 +67,8 @@ Meteor.methods({
 		Inventories.update(itemId, {
 			$set: {
 				name: name.trim(),
-				amount
+				amount,
+				listId
 			}
 		});
 	},
