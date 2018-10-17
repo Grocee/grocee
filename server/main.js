275416cf255df80f2fe0ebb1b3a794859d6dbae5
Zack Apiratitham <zack@vatthikorn.com>
2018-10-16 21:41:53 -0400

    Added support for updating user’s first/last name

--- server/main.js
+++ server/main.js
@@ -1,11 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 
 import '../imports/api/groceries';
 import '../imports/api/grocerylists';
 import '../imports/api/inventories';
 import '../imports/api/inventorylists';
 import '../imports/api/recipes';
+import '../imports/api/accounts';
 
 Meteor.startup(() => {
 	console.log('Meteor started!'); // eslint-disable-line
