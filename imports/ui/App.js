d17d3b28200fdb3b12eee4c8aeb722f03d6317ba
Zack Apiratitham <zack@vatthikorn.com>
2019-02-03 18:25:35 -0500

    Email verification and password reset
    Added UI for displaying email confirmation and form for resetting password

---
+++ imports/ui/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Session } from 'meteor/session';
+import { Accounts } from "meteor/accounts-base";
+
+export default class App extends Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.handleResetPassword = this.handleResetPassword.bind(this);
+		this.handlePassword = this.handlePassword.bind(this);
+		this.handleConfirmPassword = this.handleConfirmPassword.bind(this);
+
+		let resetPasswordToken = Session.get('resetPasswordToken');
+		let verifyEmailToken = Session.get('verifyEmailToken');
+
+		if (verifyEmailToken) {
+			Accounts.verifyEmail(verifyEmailToken, error => {
+				if (!error) {
+					this.setState({ emailVerified: true })
+				} else {
+					this.setState({ emailVerifyFailed: true, reason: error.reason })
+				}
+			});
+		}
+
+		this.state = { verifyEmailToken, resetPasswordToken };
+	}
+	
+	handlePassword(event) {
+		this.setState({ newPassword: event.target.value });
+	}
+
+	handleConfirmPassword(event) {
+		this.setState({ confirmPassword: event.target.value });
+	}
+
+	renderResetPasswordForm() {
+		return (
+			<form onSubmit={this.handleResetPassword}>
+				New Password:<br/>
+				<input type="password" onChange={this.handlePassword} /><br/>
+				Confirm New Password:<br/>
+				<input type="password" onChange={this.handleConfirmPassword} /><br/>
+				<input type="submit" value="Submit" />
+			</form>
+		);
+	}
+
+	handleResetPassword(e) {
+		e.preventDefault();
+		this.setState({ didSubmitChangePassword: true });
+		if (this.state.newPassword == this.state.confirmPassword) {
+			Accounts.resetPassword(this.state.resetPasswordToken, this.state.newPassword, error => {
+				this.setState({ changePasswordSuccess: !error });
+				if (error) {
+					this.setState({ changePasswordErrorMsg: error.reason });
+				}
+			});
+		} else {
+			this.setState({ changePasswordErrorMsg: 'Passwords do not match!' });
+			this.setState({ changePasswordSuccess: false });
+		}
+	}
+
+	renderChangePasswordResult() {
+		if (this.state.changePasswordSuccess) {
+			return (<p>Password changed successfully!</p>);
+		} else if (this.state.changePasswordErrorMsg) {
+			return (<p>{this.state.changePasswordErrorMsg}</p>);
+		}
+	}
+
+	render() {
+		return (
+			//TODO: Use formik to build the form
+			<div>
+				{this.state.emailVerified 
+					? <h3>Email Verified</h3>
+					: null
+				}
+				{this.state.emailVerifyFailed
+					? <div>
+						<h3>Email Verification Failed</h3>
+						<p>{this.state.reason}</p>
+					</div>
+					: null}
+				{this.state.resetPasswordToken
+					? this.renderResetPasswordForm()
+					: null}
+				{this.state.didSubmitChangePassword
+					? this.renderChangePasswordResult()
+					: null}
+			</div>
+		);
+	}
+}
